a4a703175f943aae4ab6b6c780a195cc1360fdf5
Jackie Guzman <jackieguzman3241@gmail.com>
2016-12-14 08:19:09 -0800

    FINISHED STEP 3, 4, 5

--- project7.js
+++ project7.js
@@ -12,30 +12,55 @@ function transformPoint(event) {
 
 // Step 2: drawSquare and drawCircle functions
 // square drawing code here
-function drawSquare(xpos, ypos, size, color) {
-  var newSquare = document.createElementNS(namespace, "square")
+function drawSquare(x, y, size, color) {
+  var newSquare = document.createElementNS(namespace, "rect")
   newSquare.setAttribute("fill", color)
   newSquare.setAttribute("width", size)
   newSquare.setAttribute("height", size)
-  newSquare.setAttribute("x", xpos)
-  newSquare.setAttribute("y", ypos)
-  canvas.appendChild(newSquare)
+  newSquare.setAttribute("x", x)
+  newSquare.setAttribute("y", y)
+  screen.appendChild(newSquare)
 
 }
 
 // circle drawing code here
-function drawCircle(xpos, ypos, size, color) {
+function drawCircle(x, y, size, color) {
   var newCircle = document.createElementNS(namespace, "circle")
   newCircle.setAttribute("fill", color)
   newCircle.setAttribute("r", size)
-  newCircle.setAttribute("cx", xpos)
-  newCircle.setAttribute("cy", ypos)
-  canvas.appendChild(newCircle)
+  newCircle.setAttribute("cx", x)
+  newCircle.setAttribute("cy", y)
+  screen.appendChild(newCircle)
 
 }
 
 // Step 3: Event listeners
 document.addEventListener("mousedown", function(e) {
+  var pt = transformPoint(e)
   // what do you want to do when the user presses down
   // on the mouse button?
+})
+
+document.addEventListener("mouseup", function(e) {
+  var pt = transformPoint(e)
+})
+  // what do you want to do when the user presses down
+  // on the mouse button?
+  // step 4 mousemove event
+ //1
+document.addEventListener("mousemove", function (e) {
+  var pt = transformPoint(e)
+  var xpos = pt.x
+  var ypos = pt.y
+  //2
+  var shapeSelect = document.getElementById("shapeSelect").value
+  var colorSelect = document.getElementById("colorSelect").value
+  var sizeSelect = document.getElementById("sizeSelect").value
+  //3
+  if (shapeSelect == "square") {
+    drawSquare(pt.x,pt.y, sizeSelect,colorSelect)
+  }
+  else if (shapeSelect == "circle" ) {
+    drawCircle(pt.x,pt.y,sizeSelect, colorSelect)
+  }
 })
